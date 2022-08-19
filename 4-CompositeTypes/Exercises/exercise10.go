8d8fc35f80915b7a1fd8d9be32fcace8474c076d
Márcio Macedo <marciocfmacedo@hotmail.com>
2022-08-18 22:48:13 -0300

    feat: Sections 4.4 and 4.5 have been added

---
+++ 4-CompositeTypes/Exercises/exercise10.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+const IssuesURL = "https://api.github.com/search/issues"
+
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
+
+// SearchIssues queries the GitHub issue tracker
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+	// We must close resp.Body on all execution paths.
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
+
+func main() {
+	result, err := SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		if time.Now().Sub(item.CreatedAt).Hours()/24/30 < 1 {
+			fmt.Print("Less than a month old - Created at: ", item.CreatedAt.String())
+		} else if time.Now().Sub(item.CreatedAt).Hours()/24/365 < 1 {
+			fmt.Print("Less than a year old - Created at: ", item.CreatedAt.String())
+		} else {
+			fmt.Print("Equal or more than a year old - Created at: ", item.CreatedAt.String())
+		}
+		fmt.Printf(" - #%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
